d19a37dab974994079892ec352ddd780729cb803
gralyan <john.gralyan@gmail.com>
2015-03-29 06:49:09 -0700

    Added GNU Affero General Public License header to each file

--- ScienceGadgets/src/com/sciencegadgets/client/algebra/transformations/AlgebraicTransformations.java
+++ ScienceGadgets/src/com/sciencegadgets/client/algebra/transformations/AlgebraicTransformations.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ *     This file is part of ScienceGadgets, a collection of educational tools
+ *     Copyright (C) 2012-2015 by John Gralyan
+ *
+ *     ScienceGadgets is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of
+ *     the License, or (at your option) any later version.
+ *
+ *     ScienceGadgets is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     Contact us at info@sciencegadgets.org
+ *******************************************************************************/
 package com.sciencegadgets.client.algebra.transformations;
 
 import java.util.LinkedHashSet;
